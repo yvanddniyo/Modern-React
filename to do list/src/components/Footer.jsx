33f914fd6285ec447edd93f2447b6bda76cb3c56
yvanddniyo <nyvan80076@gmail.com>
2023-11-01 23:21:03 +0100

    update  footer

--- to do list/src/components/Footer.jsx
+++ to do list/src/components/Footer.jsx
@@ -5,21 +5,32 @@ const Footer = () => {
       return (
             <Container>
                   <Wrap>
-                        <div>
-                              <logo>Y-Do List</logo>
-                        </div>
-                        <OurWork>
-                              <p>Build website</p>
-                              <p>Build mobile apps</p>
-                              <p>Cyber Security</p>
-                              <p>Penetration tester</p>
-                        </OurWork>
+                        <h3>Y-Do List</h3>
+                        <ul>
+                              <li>Know us</li>
+                              <li>What we do</li>
+                              <li>Our community</li>
+                              <li>Get in Touch</li>
+                        </ul>
                   </Wrap>
                   <Policy>
+                        <h3>Links</h3>
                         <ul>
-                              <li></li>
+                              <li>About</li>
+                              <li>Blog</li>
+                              <li>Careers</li>
+                              <li>Jobs</li>
                         </ul>
                   </Policy>
+                  <Developer>
+                        <h3>More</h3>
+                        <ul>
+                              <li>web</li>
+                              <li>Technology</li>
+                              <li>Learn web</li>
+                              <li>Hack Blog</li>
+                        </ul>
+                  </Developer>
             </Container>
       )
 }
@@ -27,22 +38,88 @@ const Footer = () => {
 export default Footer
 
 const Container = styled.div`
+ display: flex;
+ flex-wrap: wrap;
+ justify-content: space-around;
+
  height: 30vh;
+//  width: 100vw;
  padding: 1rem;
  background-color: gray;
 
 `
 const Wrap = styled.div`
-  div{
-      logo{
-       color: #fcfcfc;
-      font-size: 24px;
+
+      h3{
+      color: #fcfcfc;
+      font-size: 18px;
       font-weight: 700;
+      cursor: pointer;
+
+      }
+      ul{
+            padding:  1rem 0;
+            cursor: pointer;
+            font-size: 15px;
+
+        li{
+         padding: 0.5rem 0px;
+         &:hover{
+            font-size: 1.1rem;
+            font-weight: bold;
+           }
+       }
+            
       }
       
-      
-  }
+  
 `
-const OurWork = styled.div`
-   padding: 20px 0px;
+
+const Policy = styled.div`
+
+h3{
+      color: #fcfcfc;
+      font-size: 18px;
+      font-weight: 700;
+      cursor: pointer;
+
+      }
+      ul{
+            padding:  1rem 0;
+            cursor: pointer;
+            font-size: 15px;
+            transition: all 1s ease-out;
+ 
+        li{
+         padding: 0.5rem 0px;
+         &:hover{
+            font-size: 1.1rem;
+            font-weight: bold;
+           }
+       }
+            
+      }
+`
+const Developer = styled.div`
+
+h3{
+      color: #fcfcfc;
+      font-size: 18px;
+      font-weight: 700;
+      cursor: pointer;
+
+      }
+      ul{
+            padding:  1rem 0;
+            cursor: pointer;
+            font-size: 15px;
+
+        li{
+         padding: 0.5rem 0px;
+         &:hover{
+           font-size: 1.1rem;
+           font-weight: bold;
+          }
+      }
+      }
 `
